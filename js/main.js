e027166f55ff14e34f80900b026b7fb58450625d
necoop <amok2005@gmail.com>
2023-01-28 19:04:15 +0700

    Скрипты на гамбургер

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// Слайдер 1
 const swiper = new Swiper(".swiper", {
   // Optional parameters
   direction: "horizontal",
@@ -15,6 +16,7 @@ const swiper = new Swiper(".swiper", {
   },
 });
 
+// Слайдер 2
 const swiper__foto = new Swiper(".swiper__foto", {
   // Optional parameters
   direction: "horizontal",
@@ -31,3 +33,21 @@ const swiper__foto = new Swiper(".swiper__foto", {
     disableOnInteraction: false,
   },
 });
+
+// Блокируем скролл при активации гамбургера
+let hamburgerClick = document.querySelector("#menu__btn");
+hamburgerClick.addEventListener("click", scrollBlock);
+function scrollBlock() {
+  document.body.classList.toggle("noscrolling");
+}
+
+// Закрываем гамбургер при нажатии на пункт меню
+let menuBox = document.querySelectorAll(".nav__item");
+for (let i = 0; i < menuBox.length; i++) {
+  menuBox[i].addEventListener("click", menuClose);
+}
+function menuClose(click) {
+  let menuTouggle = document.querySelector("#menu__toggle");
+  menuTouggle.checked = false;
+  document.body.classList.toggle("noscrolling");
+}
